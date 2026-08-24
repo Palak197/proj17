0b444f71829ac6573840cb5b507d585127b402fb
Palak Usrethe <usrethepalak2005@gmail.com>
2026-08-24 07:26:07 +0530

    more changes

--- B.java
+++ B.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
-class B{
-    void process(){
-        System.out.println("Hello");
-    }
-}
-=======
 
->>>>>>> 9f8ac7e16746af8eddda4e5a085707de65ea36ba
+class B{
+    int y = 90;
+
+    boolean w = false;
+
+    String q = "om";.
+}
+
